e359f41acba57d643ca050f9b9c1ee4f8f47dd44
aeJimenez <ae.jimenez@mavi.mx>
2023-06-29 16:47:02 -0600

    Add more styles to Modifier

--- src/jvmMain/kotlin/com/invoice/contratista/ui/theme/Modifier.kt
+++ src/jvmMain/kotlin/com/invoice/contratista/ui/theme/Modifier.kt
@@ -4,9 +4,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.unit.dp
 
 val ModifierFieldImages = Modifier.size(24.dp)
+val ModifierFieldImagesSmall = Modifier.size(12.dp)
 val ModifierPaddingScreen = Modifier.padding(16.dp)
+val ModifierCard = Modifier.padding(8.dp)
 val ModifierPaddingScreen2 = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)
 val ModifierFill = Modifier.fillMaxWidth()
+val Alpha = Modifier.alpha(0.5f)
